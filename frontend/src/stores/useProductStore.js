e19c43e1464d19d9258f29ed146f26e7dfbca847
Rishabh Kumar Singh <rishabhkumarsingh94@gmail.com>
2025-07-24 09:05:32 +0530

    add minor changes

--- frontend/src/stores/useProductStore.js
+++ frontend/src/stores/useProductStore.js
@@ -75,6 +75,17 @@ export const useProductStore = create((set) => ({ //returns a product. yaad rakh
 			set({ loading: false });
 			toast.error(error.response.data.error || "Failed to delete product");
 		}
-    }
+    },
+	
+	fetchFeaturedProducts: async () => {
+		set({ loading: true });
+		try {
+			const response = await axios.get("/products/featured");
+			set({ products: response.data, loading: false });
+		} catch (error) {
+			set({ error: "Failed to fetch products", loading: false });
+			console.log("Error fetching featured products:", error);
+		}
+	},
 
 }))
